27995d5b39a9bcf70cad255d204402f7624b8c2b
Lily Go <lilygo7192@gmail.com>
2023-03-16 15:31:46 -0700

    Added a main index file to run from

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,17 @@
 const request = require("request");
-const args = process.argv.slice(2);
-const query = args.toString().toLowerCase().substring(0, 4);
 
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${query}`, (error, response, body) => {
-  try {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log("Couldn't find that breed! Please check for typos");
-      return;
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName.toString().substring(0, 4)}`, (error, response, body) => {
+    try {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        return callback(null, "Couldn't find that breed! Please check for typos");
+      }
+      callback(null, data);
+    } catch (error) {
+      callback(error.message);
     }
-    console.log(data, "typeOf", typeof(data));
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
